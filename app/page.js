0423f08a204435f2a8a8352380dd216fea6f7cfc
KevTheFoo <kevthefoo@gmail.com>
2025-05-09 20:04:36 +1000

    Refactor LoginForm component structure and improve button styling

--- app/page.js
+++ app/page.js
@@ -1,14 +1,18 @@
 "use client";
 
+import Link from "next/link";
 import { useState } from "react";
 import { User, KeyRound } from "lucide-react";
 
 export default function LoginForm() {
     return (
-        <form className=" border-4 flex flex-col justify-center items-center px-4 py-20">
-            <h3 className="self-center border-4">Login</h3>
+        <form className=" flex flex-col justify-center items-center px-4 py-20">
+            {/* <h3 className="self-center border-4">Login</h3> */}
             <div className="">
-                <label htmlFor="username" className="sr-only">
+                <div className="border-2 border-black rounded-xl px-4 py-2 cursor-pointer">
+                    <Link href="/dashboard">Enter</Link>
+                </div>
+                {/* <label htmlFor="username" className="sr-only">
                     Username
                 </label>
                 <input
@@ -16,7 +20,7 @@ export default function LoginForm() {
                     type="text"
                     className="w-full border-4 border-white bg-transparent pb-10"
                     placeholder="Username"
-                />
+                /> */}
                 {/* <label htmlFor="username" className="sr-only">
                     Password
                 </label>
