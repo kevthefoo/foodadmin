3d750d9b9802f20cbe008b20fe4838d7554b9a75
KevTheFoo <kevthefoo@gmail.com>
2025-05-09 19:54:30 +1000

    dashboard page 80%

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,14 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 import logo from "@/assets/logo.png";
 
 import { MdKeyboardArrowDown } from "react-icons/md";
+import {
+    FaHouseUser,
+    FaCircleUser,
+    FaList,
+    FaWrench,
+    FaHeadset,
+} from "react-icons/fa6";
 
 export default function Dashboard() {
     return (
-        <section className="border-8 border-red-500 w-screen h-screen">
-            <header className="mx-10 flex justify-center gap-2 pt-4 pb-2">
+        <section className=" w-screen h-screen flex flex-col justify-start">
+            <header className="px-10 flex justify-center gap-2 pt-4 pb-2">
                 <Image src={logo} alt="logo" width={50} height={50} />
-
                 <nav className="flex justify-between items-center w-full border-b-2 border-black">
                     <h1 className="text-2xl">DineAR</h1>
                     <div className="flex items-center">
@@ -19,6 +26,81 @@ export default function Dashboard() {
                     </div>
                 </nav>
             </header>
+            <main className=" h-full flex justify-center items-center pt-4">
+                <nav className="w-1/6 h-full border-r-2 border-black px-16 pt-4">
+                    <ul className="flex flex-col justify-start items-start">
+                        <li className="text-xl ">
+                            <Link
+                                href="/dashboard"
+                                className="flex items-center justify-center gap-2"
+                            >
+                                <div>
+                                    <FaHouseUser />
+                                </div>
+
+                                <h1>Dashboard</h1>
+                            </Link>
+                        </li>
+                        <li className="text-xl ">
+                            <Link
+                                href="/profile"
+                                className="flex items-center justify-center gap-2"
+                            >
+                                <FaCircleUser />
+                                <h1>Profile</h1>
+                            </Link>
+                        </li>
+                        <li className="text-xl ">
+                            <Link
+                                href="/menu"
+                                className="flex items-center justify-center gap-2"
+                            >
+                                <FaList />
+                                <h1>Menu</h1>
+                            </Link>
+                        </li>
+                        <li className="text-xl ">
+                            <Link
+                                href="/settings"
+                                className="flex items-center justify-center gap-2"
+                            >
+                                <FaWrench />
+
+                                <h1>Settings</h1>
+                            </Link>
+                        </li>
+                        <li className="text-xl ">
+                            <Link
+                                href="/support"
+                                className="flex items-center justify-center gap-2"
+                            >
+                                <FaHeadset />
+                                <h1>Support</h1>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+                <div className="w-full h-full px-4 flex flex-col justify-start items-start">
+                    <h1 className="text-2xl mb-4">Dashboard</h1>
+                    <div className="grid grid-cols-4 h-full w-full">
+                        <div className="border-2 border-black rounded-xl w-2/3 bg-stone-300 aspect-square flex flex-col justify-center items-center">
+                            <p>Dishes</p>
+                            <p>10</p>
+                        </div>
+                        <div className="border-2 border-black rounded-xl w-2/3 bg-stone-300 aspect-square flex flex-col justify-center items-center">
+                            <p>Pending Updates</p>
+                            <p>3</p>
+                        </div>
+                        <div className="border-2 border-black rounded-xl w-2/3 bg-stone-300 aspect-square flex flex-col justify-center items-center">
+                            <p>Recent Activity Log</p>
+                            <p>10</p>
+                        </div>
+                        <div className="border-2 border-black rounded-xl w-2/3 bg-stone-300 aspect-square flex flex-col justify-center items-center">
+                            <p className="text-9xl">+</p>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </section>
     );
 }
