0c439a0330d3a7e0ce515b09d8955e87b5a0fdc7
KevTheFoo <kevthefoo@gmail.com>
2025-05-12 21:34:04 +1000

    Update metadata for RootLayout and rename Home component for clarity

--- app/page.js
+++ app/page.js
@@ -1,17 +1,15 @@
 "use client";
-
 import Link from "next/link";
-import { useState } from "react";
-import { User, KeyRound } from "lucide-react";
 
-export default function LoginForm() {
+export default function Home() {
   return (
     <form className="flex flex-col items-center justify-center px-4 py-20">
-      {/* <h3 className="self-center border-4">Login</h3> */}
+
       <div className="">
         <div className="cursor-pointer rounded-xl border-2 border-black px-4 py-2">
           <Link href="/dashboard">Enter</Link>
         </div>
+
         {/* <label htmlFor="username" className="sr-only">
                     Username
                 </label>
